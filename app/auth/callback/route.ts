c0992dd263d286266cfa9744f678b0c72da7cc16
KhunSeenam <a0000@KhunSeenam.local>
2026-07-11 13:22:07 +0700

    Fix auth callback

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -5,10 +5,7 @@ export async function GET(request: Request) {
   const requestUrl = new URL(request.url)
   const code = requestUrl.searchParams.get('code')
   if (code) {
-    const supabase = createClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-    )
+    const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
     await supabase.auth.exchangeCodeForSession(code)
   }
   return NextResponse.redirect(new URL('/upload', requestUrl.origin))
